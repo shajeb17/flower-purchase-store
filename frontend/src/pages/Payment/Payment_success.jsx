7f970f02d7e3d1ce09d143e5454087be2524ae84
unknown <shajeb17.11@gmail.com>
2025-12-12 11:41:10 +0600

    payment-add

---
+++ frontend/src/pages/Payment/Payment_success.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Payment_success = () => {
+    return (
+        <div>
+            <h1>payment was success</h1>
+        </div>
+    );
+};
+
+export default Payment_success;
--- frontend/src/pages/PlantDetails/PlantDetails.jsx
+++ frontend/src/pages/PlantDetails/PlantDetails.jsx
@@ -1,103 +1,114 @@
-import Container from '../../components/Shared/Container'
-import Heading from '../../components/Shared/Heading'
-import Button from '../../components/Shared/Button/Button'
-import PurchaseModal from '../../components/Modal/PurchaseModal'
-import { use, useContext, useState } from 'react'
-import {useLocation } from 'react-router'
-import { AuthContext } from '../../providers/AuthContext'
-import AuthProvider from '../../providers/AuthProvider'
-import LoadingSpinner from '../../components/Shared/LoadingSpinner'
+import Container from "../../components/Shared/Container";
+import Heading from "../../components/Shared/Heading";
+import Button from "../../components/Shared/Button/Button";
+import PurchaseModal from "../../components/Modal/PurchaseModal";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { AuthContext } from "../../providers/AuthContext";
+import LoadingSpinner from "../../components/Shared/LoadingSpinner";
+import axios from "axios";
 
 const PlantDetails = () => {
-  let [isOpen, setIsOpen] = useState(false)
-  let loader=useLocation()
-  const {_id,image,name,price,quentity,catagory,description}= loader?.state || {};
-  let {user,loading}=useContext(AuthContext)
-  let {displayName,photoURL}=user || {}
- if(loading) return <LoadingSpinner></LoadingSpinner>
+  let [isOpen, setIsOpen] = useState(false);
+  let [data,setData]=useState()
+  const {_id,image,name,price,quentity,catagory,description}= data || {};
+  let params=useParams()
+  useEffect( ()=>{
+      async function asyncFun(){
+       let data=await axios.get(`http://localhost:3000/plant/${params.id}`)
+       setData(data?.data)
+     }
+     asyncFun()
+  },[params])
+  console.log(data);
   
 
+  let { user, loading } = useContext(AuthContext);
+  let { displayName, photoURL } = user || {};
+  if (loading) return <LoadingSpinner></LoadingSpinner>;
+
   const closeModal = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
 
   return (
     <Container>
-      <div className='mx-auto flex flex-col lg:flex-row justify-between w-full gap-12'>
+      <div className="mx-auto flex flex-col lg:flex-row justify-between w-full gap-12">
         {/* Header */}
-        <div className='flex flex-col gap-6 flex-1'>
+        <div className="flex flex-col gap-6 flex-1">
           <div>
-            <div className='w-full overflow-hidden rounded-xl'>
+            <div className="w-full overflow-hidden rounded-xl">
               <img
-                className='object-cover w-full'
+                className="object-cover w-full"
                 src={image}
-                alt='header image'
+                alt="header image"
               />
             </div>
           </div>
         </div>
-        <div className='md:gap-10 flex-1'>
+        <div className="md:gap-10 flex-1">
           {/* Plant Info */}
-          <Heading
-            title={name}
-            subtitle={`Category: ${catagory}`}
-          />
-          <hr className='my-6' />
+          <Heading title={name} subtitle={`Category: ${catagory}`} />
+          <hr className="my-6" />
           <div
-            className='
-          text-lg font-light text-neutral-500'
+            className="
+          text-lg font-light text-neutral-500"
           >
             {description}
           </div>
-          <hr className='my-6' />
+          <hr className="my-6" />
 
           <div
-            className='
+            className="
                 text-xl 
                 font-semibold 
                 flex 
                 flex-row 
                 items-center
                 gap-2
-              '
+              "
           >
             <div>Seller: {displayName}</div>
 
             <img
-              className='rounded-full'
-              height='30'
-              width='30'
-              alt='Avatar'
-              referrerPolicy='no-referrer'
+              className="rounded-full"
+              height="30"
+              width="30"
+              alt="Avatar"
+              referrerPolicy="no-referrer"
               src={photoURL}
             />
           </div>
-          <hr className='my-6' />
+          <hr className="my-6" />
           <div>
             <p
-              className='
+              className="
                 gap-4 
                 font-light
                 text-neutral-500
-              '
+              "
             >
               Quantity: {quentity} Units Left Only!
             </p>
           </div>
-          <hr className='my-6' />
-          <div className='flex justify-between'>
-            <p className='font-bold text-3xl text-gray-500'>Price: {price}$</p>
+          <hr className="my-6" />
+          <div className="flex justify-between">
+            <p className="font-bold text-3xl text-gray-500">Price: {price}$</p>
             <div>
-              <Button onClick={() => setIsOpen(true)} label='Purchase' />
+              <Button onClick={() => setIsOpen(true)} label="Purchase" />
             </div>
           </div>
-          <hr className='my-6' />
+          <hr className="my-6" />
 
-          <PurchaseModal closeModal={closeModal} isOpen={isOpen} />
+          <PurchaseModal
+            loader={data}
+            closeModal={closeModal}
+            isOpen={isOpen}
+          />
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default PlantDetails
+export default PlantDetails;
